2ab7d3902a21490cf39e5470e463c4f549a30c15
tensho <75622122+tensho9408@users.noreply.github.com>
2026-02-03 21:55:28 +0900

    refacor: change zod shema error message #8

--- src/app/api/payments/create-intent/route.ts
+++ src/app/api/payments/create-intent/route.ts
@@ -1,45 +1,33 @@
 import { NextRequest, NextResponse } from "next/server";
 import { StripePaymentService } from "@/infrastructure/services/StripePaymentService";
 import { createPaymentIntentSchema } from "@/infrastructure/zod/shemas";
-import z from "zod";
 
 export async function POST(request: NextRequest) {
+  const body = await request.json();
+
+  const result = createPaymentIntentSchema.safeParse(body);
+  if (!result.success) {
+    const messages = result.error.issues.map((i) => i.message).join(", ");
+    return NextResponse.json(
+      { error: "Validation Error", message: messages },
+      { status: 400 },
+    );
+  }
+
   try {
-    const body = await request.json();
-
-    // Validation
-    const validationData = createPaymentIntentSchema.parse(body);
-
     const paymentService = new StripePaymentService();
+    const response = await paymentService.createPaymentIntent(result.data);
 
-    const result = await paymentService.createPaymentIntent(validationData);
     return NextResponse.json({
       success: true,
-      data: result,
+      data: response,
     });
   } catch (error) {
-    if (error instanceof z.ZodError) {
-      const treeified = z.treeifyError(error);
-
-      return NextResponse.json(
-        {
-          error: "Validation Error",
-          errors: treeified,
-        },
-        { status: 400 },
-      );
-    }
     const errorMessage =
       error instanceof Error
         ? error.message
         : "Failed to create payment intent";
 
-    return NextResponse.json(
-      {
-        error: errorMessage,
-        ...(process.env.NODE_ENV === "development" && { details: error }),
-      },
-      { status: 500 },
-    );
+    return NextResponse.json({ error: errorMessage, status: 500 });
   }
 }
